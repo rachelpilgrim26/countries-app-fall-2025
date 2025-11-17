18e76a0d56524d6da5c322b013afa19d78dc5eb3
rachelpilgrim26 <pilgrimrachel5@gmail.com>
2025-11-17 11:17:48 -0500

    did my endpoints and also did helper functions

--- version-4/server/src/index.js
+++ version-4/server/src/index.js
@@ -1,17 +1,136 @@
 /* --------------------------------
-Server/API for Countries App Version 4
+// Server/API for Countries App Version 4
 
-DB Fiddle Link: ______________
-----------------------------------*/
-
-/*----------------------------------
-Boilerplate Code to Set Up Server
-----------------------------------*/
+// DB Fiddle Link:
+// https://www.db-fiddle.com/f/ijdjLrN7d7ZqwYauNEDpgR/9
+// ----------------------------------*/
 
 /*----------------------------------
-Helper Functions
-----------------------------------*/
+// Boilerplate Code to Set Up Server
+// ----------------------------------*/
+
+import express from "express";
+import pg from "pg";
+import config from "./config.js";
+
+const db = new pg.Pool({
+  connectionString: config.databaseUrl,
+  ssl: true,
+});
+const app = express();
+app.use(express.json());
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port #${port}`);
+});
 
 /*----------------------------------
-API Endpoints
-----------------------------------*/
+// Helper Functions
+// ----------------------------------*/
+
+async function getNewestUser() {
+  const dataText = `
+    SELECT * FROM users
+    ORDER BY user_id DESC
+    LIMIT 1
+  `;
+  const data = await db.query(dataText);
+  return data.rows;
+}
+
+// get all users from the users table
+async function getAllUsers() {
+  const dataText = `
+    SELECT * FROM users
+  `;
+  const data = await db.query(dataText);
+  return data.rows;
+}
+
+// add one user
+async function addOneUser(name, country_name, email, bio) {
+  const dataText = `
+    INSERT INTO users (name, country_name, email, bio)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+  `;
+  const data = await db.query(dataText, [name, country_name, email, bio]);
+  return data.rows[0];
+}
+
+// get all saved countries
+async function getAllSavedCountries() {
+  const dataText = `
+    SELECT * FROM saved_countries
+  `;
+  const data = await db.query(dataText);
+  return data.rows;
+}
+
+// save one country
+async function saveOneCountry(country_name) {
+  const dataText = `
+    INSERT INTO saved_countries (country_name)
+    VALUES ($1)
+    ON CONFLICT (country_name) DO NOTHING
+    RETURNING *
+  `;
+  const data = await db.query(dataText, [country_name]);
+  return data.rows[0] || null;
+}
+
+// Update one country count
+async function updateOneCountryCount(country_name) {
+  const dataText = `
+    INSERT INTO country_counts (country_name, count)
+    VALUES ($1, 1)
+    ON CONFLICT (country_name)
+    DO UPDATE SET count = country_counts.count + 1
+    RETURNING count
+  `;
+  const data = await db.query(dataText, [country_name]);
+  return data.rows[0].count;
+}
+
+/*----------------------------------
+// API Endpoints
+// ----------------------------------*/
+
+// get /get-newest-user
+app.get("/get-newest-user", async (req, res) => {
+  const newestUserRows = await getNewestUser();
+  res.json(newestUserRows);
+});
+
+// get /get-all-users
+app.get("/get-all-users", async (req, res) => {
+  const allUsers = await getAllUsers();
+  res.json(allUsers);
+});
+
+// post /add-one-user
+app.post("/add-one-user", async (req, res) => {
+  const data = req.body;
+  await addOneUser(data.name, data.country_name, data.email, data.bio);
+  res.send("Success: user was added");
+});
+
+// get /get-all-saved-countries
+app.get("/get-all-saved-countries", async (req, res) => {
+  const data = await getAllSavedCountries();
+  res.json(data);
+});
+
+// post /save-one-country
+app.post("/save-one-country", async (req, res) => {
+  const data = req.body;
+  await saveOneCountry(data.country_name);
+  res.send("Success: country was saved");
+});
+
+// post /update-one-country-count
+app.post("/update-one-country-count", async (req, res) => {
+  const data = req.body;
+  const dataCount = await updateOneCountryCount(data.country_name);
+  res.json({ count: dataCount });
+});
